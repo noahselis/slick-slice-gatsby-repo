aec285012e3b57dd028c5318b17d17617ddd3140
Noah Selis <80969784+noahselis@users.noreply.github.com>
2022-12-30 12:53:53 -0700

    piped in front page data and displayed it

---
+++ src/utils/useLatestData.js
@@ -0,0 +1,65 @@
+import { useState, useEffect } from 'react';
+
+const gql = String.raw;
+
+export default function useLatestData() {
+  // hot slices
+  const [hotSlices, setHotSlices] = useState([]);
+  // slicemasters
+  const [slicemasters, setSlicemasters] = useState([]);
+
+  // use a side effect to fetch the data from the graphql endpoint
+  useEffect(function () {
+    // when the component loads, fetch the data
+    fetch(process.env.GATSBY_GRAPHQL_ENDPOINT, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        query: gql`
+          query {
+            StoreSettings(id: "downtown") {
+              name
+              slicemaster {
+                name
+                _id
+                image {
+                  asset {
+                    url
+                    metadata {
+                      lqip
+                    }
+                  }
+                }
+              }
+              hotSlices {
+                name
+                _id
+                image {
+                  asset {
+                    url
+                    metadata {
+                      lqip
+                    }
+                  }
+                }
+              }
+            }
+          }
+        `,
+      }),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        // console.log({ res });
+        // TODO: check for errors
+        setHotSlices(res.data.StoreSettings.hotSlices);
+        setSlicemasters(res.data.StoreSettings.slicemaster);
+      })
+      .catch((err) => {
+        console.error('ERROR WITH StoreSettings FETCH', err);
+      });
+  }, []);
+  return { hotSlices, slicemasters };
+}
